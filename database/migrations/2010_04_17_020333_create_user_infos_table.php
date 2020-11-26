4694e2db9526af0f83e62b5c30adefdaafd9856e
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 08:40:30 +0700

    Create 2010_04_17_020333_create_user_infos_table.php

---
+++ database/migrations/2010_04_17_020333_create_user_infos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->string('mnv', 10);
+            $table->string('full_name', 255);
+            $table->string('name', 100);
+            $table->timestamp('birthday')->nullable()->default(null);
+            $table->timestamp('ngay_vao_lam')->nullable()->default(null);
+            $table->boolean('active')->nullable()->default(null);
+            $table->primary('mnv');
+            $table->unsignedBigInteger('created_by')->nullable()->default(null);
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+}
