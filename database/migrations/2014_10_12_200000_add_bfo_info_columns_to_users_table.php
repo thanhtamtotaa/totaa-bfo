59cea95d65e8a0c83e0377e51405a2f83cbf09ff
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 08:44:06 +0700

    Update 2014_10_12_200000_add_bfo_info_columns_to_users_table.php

---
+++ database/migrations/2014_10_12_200000_add_bfo_info_columns_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTwoFactorColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('info_mnv', 10)
+                    ->after('password')
+                    ->nullable()
+                    ->default(null);
+            $table->foreign('info_mnv')->references('mnv')->on('user_infos')->onDelete('SET NULL')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['info_mnv']);
+            $table->dropColumn('info_mnv');
+        });
+    }
+}
