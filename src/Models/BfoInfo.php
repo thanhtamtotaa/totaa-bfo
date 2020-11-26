6068dfc0378fbfd6badb2d9671058b107afbdf85
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 08:37:11 +0700

    Update BfoInfo.php

--- src/Models/BfoInfo.php
+++ src/Models/BfoInfo.php
@@ -4,11 +4,69 @@ namespace Totaa\TotaaBfo\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wildside\Userstamps\Userstamps;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
-class BfoInfo extends Model
+class BfoInfo extends Model implements AuthorizableContract
 {
-  use HasFactory;
+    use HasFactory;
+    use SoftDeletes;
+    use Userstamps;
+    use Authorizable;
 
-  // Disable Laravel's mass assignment protection
-  protected $guarded = [];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mnv', 'full_name', 'name', 'birthday', 'ngay_vao_lam', 'active',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_infos';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'mnv';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'birthday' => 'datetime',
+        'ngay_vao_lam' => 'datetime',
+    ];
+
+    /**
+     * guard_name
+     *
+     * @var string
+     */
+    protected $guard_name = 'web';
 }
