9afb9652cb2620ec0cd1d0ecd01498a91bf89a3e
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-01 09:43:44 +0700

    Create BfoInfoDataTable.php

---
+++ src/DataTables/BfoInfoDataTable.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Totaa\TotaaBfo\DataTables;
+
+use Totaa\TotaaBfo\Models\BfoInfo;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class BfoInfoDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', 'bfoinfo.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \Totaa\TotaaBfo\Models\BfoInfo $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(BfoInfo $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('bfoinfo-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(60)
+                  ->addClass('text-center'),
+            Column::make('id'),
+            Column::make('add your columns'),
+            Column::make('created_at'),
+            Column::make('updated_at'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'BfoInfo_' . date('YmdHis');
+    }
+}
