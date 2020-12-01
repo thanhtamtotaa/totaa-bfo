1886716a855996537ec321c7384cf0ad69921549
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-01 17:14:44 +0700

    Update bfo.blade.php

--- resources/views/bfo.blade.php
+++ resources/views/bfo.blade.php
@@ -3,12 +3,15 @@
 @section('styles')
     <link rel="stylesheet" href="{{ mix('/vendor/libs/datatables/datatables.css') }}">
     <link rel="stylesheet" href="{{ mix('/vendor/libs/select2/select2.css') }}">
+    <link rel="stylesheet" href="{{ mix('/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.css') }}">
 @endsection
 
 @section('scripts')
     <!-- Dependencies -->
     <script src="{{ mix('/vendor/libs/datatables/datatables.js') }}"></script>
     <script src="{{ mix('/vendor/libs/select2/select2.js') }}"></script>
+    <script src="{{ mix('/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ mix('/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.vi.min.js') }}"></script>
 @endsection
 
 @push('datatables')
