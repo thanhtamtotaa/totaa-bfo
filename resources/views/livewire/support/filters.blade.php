50d3ae4ee141e0f1831a458c7db51f3107073ce0
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-09 18:14:24 +0700

    Update filters.blade.php

--- resources/views/livewire/support/filters.blade.php
+++ resources/views/livewire/support/filters.blade.php
@@ -2,7 +2,7 @@
 <div class="px-4 pt-4 mb-0" wire:ignore>
     <div class="form-row">
 
-        @if (Auth::user()->bfo_info->can("add-team"))
+        @if (Auth::user()->bfo_info->can("add-bfo"))
             <div class="col-md-auto mb-4 pr-md-3">
                 <label class="form-label d-none d-md-block">&nbsp;</label>
                 <div class="col px-0 mb-1 text-md-left text-center">
